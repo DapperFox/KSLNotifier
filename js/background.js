55c559da79cd84bb3866ae132cf824ee2eaf78a2
Blake Bishop <blake@nuvi.com>
2015-08-24 18:46:41 -0600

    Tabs to spaces

--- js/background.js
+++ js/background.js
@@ -1,10 +1,10 @@
-//	Runs the notification on ready and sets the interval every to poll every so often
+//  Runs the notification on ready and sets the interval every to poll every so often
 var sharedFunctions = window.sharedFunctions || {};
 $(document).ready(function(){
-	sharedFunctions.startNotifications();
-	var interval = setInterval(sharedFunctions.startNotifications, 30000);
-	chrome.notifications.onClicked.addListener(sharedFunctions.notificationId);
-	if(!localStorage.count) {
-		localStorage.count = 0;
-	}
+  sharedFunctions.startNotifications();
+  var interval = setInterval(sharedFunctions.startNotifications, 30000);
+  chrome.notifications.onClicked.addListener(sharedFunctions.notificationId);
+  if(!localStorage.count) {
+    localStorage.count = 0;
+  }
 });
--- js/options.js
+++ js/options.js
@@ -1,41 +1,41 @@
 var options = window.options || {};
 var sharedFunctions = window.sharedFunctions || {};
 (function (namespace) {
-	//	Gets the values in the dropdowns and sets them to local storage.
-	//	Also recalls shared.startNotifications so that the notifications will be updated instantly.
-	namespace.saveSelection = function() {
-		var primary = $('#category').val();
-		var secondary = $('#'+primary).val();
-		var filter = $('#FilterTerm').val();
-		if(filter !== '') {
-			chrome.storage.local.set({ 'filterTerm': filter });
-		}
-		chrome.storage.local.set({ 'selectedCategory': primary + ','+secondary });
-		sharedFunctions.startNotifications();
-	};
+  //  Gets the values in the dropdowns and sets them to local storage.
+  //  Also recalls shared.startNotifications so that the notifications will be updated instantly.
+  namespace.saveSelection = function() {
+    var primary = $('#category').val();
+    var secondary = $('#'+primary).val();
+    var filter = $('#FilterTerm').val();
+    if(filter !== '') {
+      chrome.storage.local.set({ 'filterTerm': filter });
+    }
+    chrome.storage.local.set({ 'selectedCategory': primary + ','+secondary });
+    sharedFunctions.startNotifications();
+  };
 
-	//	Loops through the LookupCategories in ksl_categories and adds them to the dropdown
-	//	when a different one is selected.
-	namespace.insertSubCategories = function(select){
-		var output = "";
-		for(var prop in LookupCategories[select])
-		{
-			output += '<option value="' + prop + '">' + prop + '</option>';
-		}
-		$('#'+select).html(output);
-	};
+  //  Loops through the LookupCategories in ksl_categories and adds them to the dropdown
+  //  when a different one is selected.
+  namespace.insertSubCategories = function(select){
+    var output = "";
+    for(var prop in LookupCategories[select])
+    {
+      output += '<option value="' + prop + '">' + prop + '</option>';
+    }
+    $('#'+select).html(output);
+  };
 
-	//	Shows the dropdown by the id of the category dropdown and hides the rest.
-	//	Adds the listener to the save click event.
-	$(document).ready(function(){
-		$('#category').change(
-			function(){
-				$('.secondary').hide();
-				var selectedItem = '#' + $(this).val();
-			$(selectedItem).show();
-			//	Inserts the categories into the dropdown
-			namespace.insertSubCategories($(this).val());
-			});
-		$('#Save').click(namespace.saveSelection);
-	});
+  //  Shows the dropdown by the id of the category dropdown and hides the rest.
+  //  Adds the listener to the save click event.
+  $(document).ready(function(){
+    $('#category').change(
+      function(){
+        $('.secondary').hide();
+        var selectedItem = '#' + $(this).val();
+      $(selectedItem).show();
+      //  Inserts the categories into the dropdown
+      namespace.insertSubCategories($(this).val());
+      });
+    $('#Save').click(namespace.saveSelection);
+  });
 })(options);
